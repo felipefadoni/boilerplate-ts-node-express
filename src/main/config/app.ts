9789817814273cdf0112da78f783ecd24f4c3b71
Felipe Fadoni <felipe.fadoni@gsw.com.br>
2021-09-22 20:27:45 -0300

    chore: configure helmet cors morgan compression

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,11 +1,25 @@
 import express from 'express'
+import morgan from 'morgan'
+import helmet from 'helmet'
+import compression from 'compression'
+import cors from 'cors'
+
 import { resolve } from 'path'
 import { setupRoutes } from '@/app/routes'
 
 const app = express()
 
+app.use(
+  morgan(
+    '"IP :remote-addr" - ":method :url" "STATUS :status" ":referrer" :response-time ms',
+    { skip: () => process.env.NODE_ENV === 'test' }
+  )
+)
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+app.use(helmet())
+app.use(compression())
+app.use(cors())
 
 app.get('/', async (request, response) => {
   const { version, license } = await import(
